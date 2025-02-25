6d65ab8a39ac619f4eef3d79ceca95c70e028894
ggang89 <ggang89@gmail.com>
2025-02-25 20:41:24 +0900

    catchAll 페이지는 default 페이지와 달리 링크로 이동해도, 새로고침 전과  같은 catchAll페이지 보여짐

---
+++ src/app/board/@boardList/[...catchAll]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//default page는 nav로 이동시 새로고침해야 보여지는데
+// catchAll page는 nav로 이동시 새로고침 없이 보여짐
+// => default와 달리 새로고침해도 계속 똑같은 페이지 보여짐
+export default function BoardListCatchAllPage() {
+  return (
+    <div>💫BoardListCatchAllPage</div>
+  )
+}
