a3ede72b4bc1b2c093c243642bef1aed3089b21e
ggang89 <ggang89@gmail.com>
2025-02-25 20:02:41 +0900

    조건처리 => isAdmin이 true일 때만 보이는 admin page를 보여줌

--- src/app/board/layout.tsx
+++ src/app/board/layout.tsx
@@ -5,17 +5,24 @@ export default function BoardLayout({
   children,
   boardList,
   comments,
+  admin
 }: {
   children: ReactNode;
   boardList: ReactNode;
   comments: ReactNode;
-}) {
+  admin:ReactNode;
+  }) {
+  const isAdmin = false;
   return (
     <div >
+      {/* 조건처리=> isAdmin이 true일 때만 admin 보여줌 */}
+      {isAdmin && admin} 
+
       {children}
       {boardList}
       {comments}
-    </div>);
+    </div>
+  );
 }
 
 
